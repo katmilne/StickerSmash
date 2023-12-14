fca83022978313d162440312e4c68743c47f4192
katmilne <katmilne39@gmail.com>
2023-12-14 22:49:43 +0000

    Implemented sticker size and location gestures

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import CircleButton from './components/CircleButton';
 import IconButton from './components/IconButton';
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -50,7 +51,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
@@ -74,7 +75,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} /> 
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+      </GestureHandlerRootView>
   );
 }
 
